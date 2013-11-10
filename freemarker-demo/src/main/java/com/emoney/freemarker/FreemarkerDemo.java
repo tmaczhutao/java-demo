7ec3e047ecccbdc02dd28d3f9826cb1ba04dea8c
tmaczhutao <tmaczhutao@163.com>
2013-11-10 22:09:07 +0800

    test

---
+++ freemarker-demo/src/main/java/com/emoney/freemarker/FreemarkerDemo.java
@@ -0,0 +1,12 @@
+package com.emoney.freemarker;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: thomas
+ * Date: 13-11-10
+ * Time: 下午9:59
+ * To change this template use File | Settings | File Templates.
+ */
+public class FreemarkerDemo {
+
+}
